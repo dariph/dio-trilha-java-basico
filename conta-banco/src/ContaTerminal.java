ac9c19893348055a4e9b3f315026ff22f0806259
dariph <dariph27@gmail.com>
2025-02-12 15:14:50 -0300

    codigo conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+
+        
+        //int numero =1021;
+        //String agencia = "067-8";
+        String nomeCliente = "Mario Andrade";
+        double saldo = 237.48; 
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da agencia");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o numero da conta");
+        int numero = scanner.nextInt();
+        System.out.println("Olá "+nomeCliente+" obrigado por criar uma conta em nosso banco, sua agencia é "+ agencia + " conta  " + numero + " e seu saldo " + saldo +" já está disponivel para saque");
+        scanner.close();
+
+    }   
+
 }
