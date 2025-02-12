232a47769acb36b9d5edafb509e3d14cb903176a
Dari Pinheiro <contatodariph27@gmail.com>
2025-02-12 15:59:23 -0300

    Revert "alteração arquiv"
    This reverts commit ba61722c767f702a3f72c42f842d97cc187ee281.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
+
+        
         //int numero =1021;
         //String agencia = "067-8";
-
         String nomeCliente = "Mario Andrade";
         double saldo = 237.48; 
         
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Por favor, digite o numero da agencia: ");
+        System.out.println("Por favor, digite o numero da agencia");
         String agencia = scanner.nextLine();
-
-        
-        System.out.print("Por favor, digite o numero da conta: ");
+        System.out.println("Por favor, digite o numero da conta");
         int numero = scanner.nextInt();
-        
-        System.out.println("---------------------------------");
-        System.out.println("Ola "+ nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é "+ agencia + " conta  " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque.");
-        System.out.println("---------------------------------");
-        
+        System.out.println("Olá "+nomeCliente+" obrigado por criar uma conta em nosso banco, sua agencia é "+ agencia + " conta  " + numero + " e seu saldo " + saldo +" já está disponivel para saque");
         scanner.close();
-    } 
+
+    }   
+
 }
